41178051f46c96352af1ffdf2d78932d55147bef
saywhite <saywhite@umich.edu>
2014-09-27 20:39:35 -0700

    20140927

---
+++ antelope.tcl
@@ -0,0 +1,151 @@
+#!/usr/bin/tclsh
+#############################################################################
+#
+# Antelope Studio
+#
+# Advanced Analyze System for Individual Investor
+#
+# Revisions:
+# Sept 09/21, 2014  initial revision 0.1   last modified by YoungY
+#############################################################################
+#lappend auto_path /home/saywhite/antelope/dev/packages
+
+set release true
+#set release false
+
+if {$release} {
+   package provide antelope 1.0
+   package require toolbox
+} else {
+   source toolbox/readline.tcl
+   source toolbox/canvas_zooming.tcl
+}
+
+proc init_antelope {} {
+     verinfo
+ 	 tclline
+     register_alias
+     fileevent stdin readable tclline
+     vwait forever
+     doExit
+}
+
+proc verinfo {} {
+	 set systemTime [clock seconds]
+	 puts ">> +++ [ESC]\[1;93mAntelope Studio[ESC]\[0m +++"
+	 puts ">> +++ [ESC]\[1;91mVersion 0.1[ESC]\[0m | Advanced Analyze System for Individual Inverstors | [clock format $systemTime -format {%D %H:%M:%S}]"
+	 puts ">> +++ Machine: $::tcl_platform(machine) | OS: $::tcl_platform(os) $::tcl_platform(osVersion) | Platform: $::tcl_platform(platform)"
+}
+
+proc register_alias {} {
+	 #alias oa open_analyzer
+     # Custom Commands 
+	 alias ll [list ls -ltr]
+	 alias g  [list gvim]
+}
+
+proc source_opt {file} {
+     if {info exists file} {
+		 source $file
+     }
+}
+
+proc open_analyzer {} {
+     package require Tk
+#     package require BWidget
+
+	 global globalFont
+	 set globalFont [list -family system -size 9]
+
+	 # Window title
+     wm title . "Antelope Studio"
+
+	 # Menu Bar Frame
+	 frame .mbar -borderwidth 1 -relief raised 
+	 pack .mbar -fill x -side top
+	 topmenu_create .mbar left "File"            "Open Exit"
+	 topmenu_create .mbar left "Options"         "Open Exit"
+	 topmenu_create .mbar left "Account Profile" "Open Exit"
+	 topmenu_create .mbar left "General Market"  "Open Exit"
+	 topmenu_create .mbar left "Stock Category"  "Open Exit"
+	 topmenu_create .mbar left "Short Term"      "Open Exit"
+	 topmenu_create .mbar left "Commodity"       "Open Exit"
+	 topmenu_create .mbar left "Manual"          "Open Exit"
+
+	 # 2nd level menu bar
+     frame .style -borderwidth 1 -relief sunken -relief flat
+	 pack .style -fill x
+	 label .style.readout -text "x: 0.00 y: 0.00" -font "$globalFont" 
+	 pack .style.readout -side right
+
+	 cmenu_create .style.color "Color" {set ::color}
+     pack .style.color -side left  -ipadx 18 -ipady 2
+
+     cmenu_create .style.bg "Background" {.sketchpad configure -bg}
+	 pack .style.bg -side left  -ipadx 18 -ipady 2
+
+     # Info Panel Frame
+	 frame .infopanel -highlightthickness 0 -borderwidth 1 -relief flat -width 250 -bg white  
+
+	 # Tool Panel
+	 frame .toolpanel -highlightthickness 0 -borderwidth 1 -relief groove -width 40 -bg white  
+
+	 pack  .infopanel .toolpanel -fill y -side left 
+
+	 # Canvas
+	 init_canvas .sketchpad 1280 900
+     set ::color "black"
+
+	 # display pen location
+	 bind .sketchpad <Motion> {sketch_coords %x %y; drawMarker .sketchpad  %x %y}
+     bind .sketchpad <ButtonPress-1> {sketch_box_add %x %y}
+	 bind .sketchpad <B1-Motion>     {sketch_box_add %x %y}
+}
+
+# First Level Menu Bar
+proc topmenu_create {parent side item {options ""}} {
+	 global globalFont
+	 set text $item
+	 set item [string tolower $item]
+	 menubutton ${parent}.${item} -text $text -font "$globalFont" -underline 0 -menu ${parent}.${item}.m
+	 pack ${parent}.${item} -side $side -ipadx 18 -ipady 4
+	 menu ${parent}.${item}.m -tearoff 0
+	 foreach option $options {
+		  ${parent}.${item}.m add command -label $option -underline 0 -font "$globalFont"
+     }
+}
+
+# Secondary Menu
+
+# Color Menu
+proc cmenu_create {win title cmd} {
+	 global globalFont
+	 menubutton $win -text $title -font "$globalFont" -menu $win.m
+	 menu $win.m
+	 $win.m add command -label "Red"   -font "$globalFont" -command "$cmd red"
+ 	 $win.m add command -label "Blue"  -font "$globalFont" -command "$cmd blue"
+	 $win.m add command -label "White" -font "$globalFont" -command "$cmd white"
+	 $win.m add command -label "Black" -font "$globalFont" -command "$cmd black"
+}
+
+proc sketch_coords {x y} {
+	 global globalFont
+     set size [winfo fpixels .sketchpad 1i]
+   	 set x [expr $x/$size]
+	 set y [expr $y/$size]
+     .style.readout configure -text [format "x: %6.2fi  y: %6.2fi" $x $y] -font "$globalFont" 
+}
+
+proc sketch_box_add {x y} {
+	 set x0 [expr $x-1]
+	 set x1 [expr $x+1]
+	 set y0 [expr $y-1]
+	 set y1 [expr $y+1]
+	 .sketchpad create rectangle $x0 $y0 $x1 $y1 -outline "" -fill $::color
+}
+
+proc draw_stock {} {
+     
+}
+
+init_antelope
---
+++ toolbox/canvas_zooming.tcl
@@ -0,0 +1,158 @@
+package provide toolbox 1.0
+
+# canvas initialization
+proc init_canvas {area area_width area_height} {
+     global xc yc xoffset yoffset scalefactor orig_width orig_height
+	 set xc 0
+	 set yc 0
+	 set xoffset 0.0
+	 set yoffset 0.0
+	 set scalefactor 1.0
+     set orig_width  $area_width
+	 set orig_height $area_height
+
+	 set c [canvas $area -highlightthickness 0 -borderwidth 0 -width $area_width -height $area_height -background white] 
+	 pack $c -fill both -expand 1
+	 focus $c
+
+     # Set up event bindings for canvas:
+	 bind $c <Button-2>                "set xc %x;  set yc %y" 
+     bind $c <B2-Motion>               "moveItems  $c   %x %y"
+     bind $c <Button-4>                "scaleItems $c + "
+     bind $c <Button-5>                "scaleItems $c - "
+     bind $c <Control-KeyPress-plus>   "scaleItems $c + "
+     bind $c <Control-KeyPress-minus>  "scaleItems $c - "
+	 bind $c <3>                       "zoomMark   $c   %x %y"
+     bind $c <B3-Motion>               "zoomStroke $c   %x %y"
+     bind $c <ButtonRelease-3>         "zoomArea   $c   %x %y"
+     bind $c <KeyPress-f>              "resetView  $c   "
+}
+
+proc drawMarker {c x y} {
+     set width  [winfo width $c]
+     set height [winfo height $c]
+     $c delete verticalMarker
+	 $c delete horizontalMarker
+	 $c create line $x 0 $x $height -width 1 -dash {,} -fill red -tag verticalMarker
+	 $c create line  0 $y $width $y -width 1 -dash {,} -fill red -tag horizontalMarker
+}
+
+proc resetView {c} {
+	 global orig_width orig_height
+     set width  [winfo width $c]
+     set height [winfo height $c]
+	 set xoffset_orig [expr ($orig_width -$width )/2.0] 
+	 set yoffset_orig [expr ($orig_height-$height)/2.0] 
+     global xoffset yoffset scalefactor
+     $c scale all [expr $orig_width/2.0] [expr $orig_height/2.0] [expr 1.0/$scalefactor] [expr 1.0/$scalefactor]
+	 $c move all [expr -$xoffset-$xoffset_orig] [expr -$yoffset-$yoffset_orig] 
+	 set xoffset 0.0
+	 set yoffset 0.0
+	 set scalefactor 1.0
+	 set orig_width  $width
+	 set orig_height $height
+}
+
+proc moveItems {c x y } {
+     global xc yc xoffset yoffset scalefactor
+     $c move all [expr {$x-$xc}] [expr {$y-$yc}]
+	 set xoffset [expr $xoffset + ($x-$xc)/$scalefactor]
+	 set yoffset [expr $yoffset + ($y-$yc)/$scalefactor]
+     set xc $x
+     set yc $y
+}
+
+proc scaleItems {c type} {
+	 global orig_width orig_height
+     set width  [winfo width $c]
+     set height [winfo height $c]
+ 	 set xoffset_orig [expr ($orig_width -$width )/2.0] 
+	 set yoffset_orig [expr ($orig_height-$height)/2.0] 
+	 global scalefactor
+     if { $type eq "+" } {
+		 set scalefactor [expr $scalefactor*sqrt(2.0)]
+         $c scale all [expr $orig_width/2.0] [expr $orig_height/2.0] [expr {sqrt(2.0)}] [expr {sqrt(2.0)}]
+     } else {
+		 set scalefactor [expr $scalefactor/sqrt(2.0)]
+         $c scale all [expr $orig_width/2.0] [expr $orig_height/2.0] [expr {1.0/sqrt(2.0)}] [expr {1.0/sqrt(2.0)}]
+     } 
+	 $c move all [expr -$xoffset_orig] [expr -$yoffset_orig]
+ 	 set orig_width  $width
+	 set orig_height $height
+}
+
+proc zoomMark {c x y} {
+    global zoomArea
+    set zoomArea(x0) $x
+    set zoomArea(y0) $y
+    $c create rectangle $x $y $x $y -dash {.} -outline blue -tag zoomArea
+}
+
+proc zoomStroke {c x y} {
+    global zoomArea
+    set zoomArea(x1) $x
+    set zoomArea(y1) $y
+    $c coords zoomArea $zoomArea(x0) $zoomArea(y0) $zoomArea(x1) $zoomArea(y1)
+}
+
+proc zoomArea {c x y} {
+    global zoomArea scalefactor xc yc xoffset yoffset 
+
+    #--------------------------------------------------------
+    #  Get the final coordinates.
+    #  Remove area selection rectangle
+    #--------------------------------------------------------
+    set zoomArea(x1) $x
+    set zoomArea(y1) $y
+    $c delete zoomArea
+
+    #--------------------------------------------------------
+    #  Check for zero-size area
+    #--------------------------------------------------------
+    if {($zoomArea(x0)==$zoomArea(x1)) || ($zoomArea(y0)==$zoomArea(y1))} {
+        return
+    }
+
+    #--------------------------------------------------------
+    #  Determine size and center of selected area
+    #--------------------------------------------------------
+    set areaxlength [expr {abs($zoomArea(x1)-$zoomArea(x0))}]
+    set areaylength [expr {abs($zoomArea(y1)-$zoomArea(y0))}]
+    set xcenter [expr {($zoomArea(x0)+$zoomArea(x1))/2.0}]
+    set ycenter [expr {($zoomArea(y0)+$zoomArea(y1))/2.0}]
+
+
+    #--------------------------------------------------------
+    #  Determine size of current window view
+    #  Note that canvas scaling always changes the coordinates
+    #  into pixel coordinates, so the size of the current
+    #  viewport is always the canvas size in pixels.
+    #  Since the canvas may have been resized, ask the
+    #  window manager for the canvas dimensions.
+    #--------------------------------------------------------
+    set width  [winfo width $c]
+    set height [winfo height $c]
+
+    #--------------------------------------------------------
+    #  Calculate scale factors, and choose smaller
+    #--------------------------------------------------------
+    set xscale [expr {1.0*$width/$areaxlength}]
+    set yscale [expr {1.0*$height/$areaylength}]
+    if { $xscale > $yscale } {
+        set factor $yscale
+    } else {
+        set factor $xscale
+    }
+    set factor [expr $factor*$scalefactor]
+
+    #--------------------------------------------------------
+    #  Perform zoom operation
+    #--------------------------------------------------------
+	set xc [expr ($xcenter-$width /2.0)/$scalefactor-$xoffset]
+	set yc [expr ($ycenter-$height/2.0)/$scalefactor-$yoffset]
+	resetView $c 
+	moveItems $c 0.0 0.0
+	$c scale all [expr $width/2.0] [expr $height/2.0] [expr 1.0*$factor] [expr 1.0*$factor]
+	set scalefactor $factor
+}
+
